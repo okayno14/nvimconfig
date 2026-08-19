5b67a5aa3ad9f10d82cdeae0be0e797370852365
Aleksandr Alekseev <aleksandr.alekseev@eltex.loc>
2026-08-19 10:35:10 +0700

    Добавлен lockfile для pckr

---
+++ .config/nvim/pckr/lockfile.lua
@@ -0,0 +1,13 @@
+return {
+  ["https://github.com/MunifTanjim/nui.nvim"] = { commit = "de740991c12411b663994b2860f1a4fd0937c130" },
+  ["https://github.com/chentoast/marks.nvim"] = { commit = "f353e8c08c50f39e99a9ed474172df7eddd89b72" },
+  ["https://github.com/denialofsandwich/sudo.nvim"] = { commit = "9ab546adfea5331d24cb6086a7a97c5ec8d5b0b2" },
+  ["https://github.com/harrisoncramer/gitlab.nvim"] = { commit = "e29909cd1064a7b53c3150bff49449a548dadf8d" },
+  ["https://github.com/mitander/flume.nvim"] = { commit = "2ee070038817878463a3e0d0556f4ca3ef043c4a" },
+  ["https://github.com/necrogoru/shades-of-purple.nvim"] = { commit = "d403d33f14b0233f9ccc5371668cfea7ed125ce4" },
+  ["https://github.com/nvim-lua/plenary.nvim"] = { commit = "b9fd5226c2f76c951fc8ed5923d85e4de065e509" },
+  ["https://github.com/nvim-tree/nvim-web-devicons"] = { commit = "8dcb311b0c92d460fac00eac706abd43d94d68af" },
+  ["https://github.com/projekt0n/caret.nvim"] = { commit = "62a18089e5eedc329dea7fb5f1436d1c1bf672b1" },
+  ["https://github.com/sindrets/diffview.nvim"] = { commit = "4516612fe98ff56ae0415a259ff6361a89419b0a" },
+  ["https://github.com/stevearc/dressing.nvim"] = { commit = "2d7c2db2507fa3c4956142ee607431ddb2828639" },
+}
